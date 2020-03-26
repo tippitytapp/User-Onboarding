177fa7daddc7f90218617f266412bed56f6b9fa4
tippitytapp <60143534+tippitytapp@users.noreply.github.com>
2020-03-26 11:36:27 -0400

    finished the errors

--- formilkandyup/src/App.js
+++ formilkandyup/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import * as yup from "yup";
 import SignUpForm from "./components/Form";
 import './App.css';
 
+
+
+
 function App() {
+
+
+
   return (
     <div className="App">
       <img src="./images/yuplarge.JPG" alt="the word 'Yup' written in cursive" />
--- formilkandyup/src/components/Form.js
+++ formilkandyup/src/components/Form.js
@@ -1,29 +1,138 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import * as yup from "yup";
 
-
-
-
-
+const schema = yup.object().shape({
+    fname: yup.string().required("First Name is required"),
+    lname: yup.string().required("Last Name is required"),
+    email: yup.string().email("Must be a valid email").required("Email is required"),
+    pword: yup.string().required("Password Required"),
+    terms: yup.boolean().oneOf([true], "Please agree to the terms and conditions to continue")
+})
 
 function SignUpForm(props){
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [formState, setFormState] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        pword: "",
+        terms: ""
+      });
+    const [errors, setErrors] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        pword: "",
+        terms: ""
+    });
+    const [post, setPost] = useState([]);
+
+    useEffect(()=> {
+        schema.isValid(formState).then(valid => {
+            setButtonDisabled(!valid);
+        });
+    },[formState]);
+
+    const formSubmit = e => {
+        e.preventDefault();
+        axios
+        .post("https://reqres.in/api/users", formState)
+        .then(res => {
+            setPost(res.data);
+            console.log("success", post);
+            setFormState({
+                fname: "",
+                lname: "",
+                email: "",
+                pword: "",
+                terms: ""
+            })
+        })
+        .catch(err => console.log(err.response));
+    };
+
+    const validateChange = e => {
+        yup
+        .reach(schema, e.target.name)
+        .validate(e.target.value)
+        .then(valid => {
+            setErrors({
+                ...errors, [e.target.name]: ""
+            });
+        })
+        .catch(err => {
+            setErrors({
+                ...errors, [e.target.name]: err.errors[0]
+            });
+        });
+    };
+
+    const inputChange = e => {
+        e.persist();
+        const newFormData = {
+            ...formState, [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value
+        };
+        validateChange(e);
+        setFormState(newFormData);
+
+    }
+
+
     return (
-<form>
+<form onSubmit={formSubmit}>
     <label htmlFor="fname">First Name: </label>
-    <input type="text" name="fname" id="fname" placeholder="First Name" />
+    <input 
+    type="text" 
+    name="fname" 
+    id="fname"
+    value={formState.fname}
+    onChange={inputChange}
+    placeholder="First Name" 
+    />
+    {errors.fname.length > 0 ? <p className="error">{errors.fname}</p> : null}
     <br />
     <label htmlFor="lname">Last Name: </label>
-    <input type="text" name="lname" id="lname" placeholder="Last Name" />
+    <input 
+    type="text" 
+    name="lname" 
+    id="lname" 
+    value={formState.lname}
+    onChange={inputChange} 
+    placeholder="Last Name" />
+     {errors.lname.length > 0 ? <p className="error">{errors.lname}</p> : null}
     <br />
     <label htmlFor="email">Email: </label>
-    <input type="text" name="email" id="email" placeholder="Email" />
+    <input 
+    type="email" 
+    name="email" 
+    id="email" 
+    value={formState.email}
+    onChange={inputChange} 
+    placeholder="Email" />
+     {errors.email.length > 0 ? <p className="error">{errors.email}</p> : null}
     <br />
     <label htmlFor="pword">Password: </label>
-    <input type="password" name="pword" id="pword" placeholder="Password" />
+    <input 
+    type="password" 
+    name="pword" 
+    id="pword" 
+    value={formState.pword}
+    onChange={inputChange} 
+    placeholder="Password" 
+    />
+     {errors.pword.length > 0 ? <p className="error">{errors.pword}</p> : null}
     <br />
     <label htmlFor="terms">Clicking "Submit" means that you agree to the terms and conditions of use.</label>
-    <input type="checkbox" />
+    <input 
+    type="checkbox"
+    name="terms"
+    checked={formState.terms}
+    onChange={inputChange}
+    />
     <br />
-    <button>Submit</button>
+    <pre>{JSON.stringify(post, null, 2)}</pre>
+    <button disabled={buttonDisabled}>Submit</button>
 
 </form>
     )
